79e22245569142be2199a27de43f9f43addfd0f8
MihajloESTS <milojevicm374@gmail.com>
2022-01-15 21:32:21 +0100

    Basic Node and Express - Done

--- Back End Development and APIs/Basic Node and Express/myApp.js
+++ Back End Development and APIs/Basic Node and Express/myApp.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
-
-
 var express = require('express');
 var app = express();
+const bodyParser = require("body-parser");
 
 console.log("Hello World");
 /*
@@ -12,6 +11,12 @@ app.get("/", (req, res) => {
 */
 
 app.use("/public", express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} - ${req.ip}`)
+  next()
+})
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html")
@@ -22,5 +27,25 @@ app.get("/json", (req, res) => {
     return res.json({"message": "Hello json".toUpperCase()})
   res.json({message: "Hello json"});
 })
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({time: req.time})
+})
+
+app.get("/:word/echo", (req, res) => {
+  res.json({echo: req.params.word});
+})
+
+app.route("/name")
+  .get((req, res) => {
+    res.json({name: `${req.query.first} ${req.query.last}`});
+  })
+  .post((req, res) => {
+    res.json({name: `${req.body.first} ${req.body.last}`});
+  })
+
 
 module.exports = app;
